7a9cd93aca8964883f7c9298949aad5b4cc6b7d3
Jurgen Mejias <jurgen.mejias23@gmail.com>
2024-01-29 21:11:02 -0600

    fix

--- config/db.js
+++ config/db.js
@@ -10,7 +10,7 @@ const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env
         timestamps: true
     },
     pool: {
-        max: 5, //maximo de conexiones por usuario
+        max: 10, //maximo de conexiones por usuario
         min: 0, //minimo de conexiones por usuario
         acquire: 30000, //tiempo antes de marcar un error (en milisegundos)
         idle: 10000 //tiempo para liberar conexiones que no se esten usando (en milisegundos)
